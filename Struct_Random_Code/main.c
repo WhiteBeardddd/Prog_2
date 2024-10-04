17ab7e4899526796b3a8350999c91ed7027b8222
WhiteBeardddd <charlesbenedictboquecosa@gmail.com>
2024-10-04 16:43:19 +0800

    Midterm

--- Struct_Random_Code/main.c
+++ Struct_Random_Code/main.c
@@ -2,143 +2,194 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 50
+#define NUM_GRADES 5
 
 typedef struct{
-    char fname[MAX];
-    char lname[MAX];
-    char mi;
-    int age;
-}Student;
+    char name[MAX];
+    int studentID;
+    float grade[NUM_GRADES];
+}StudentList;
 
+void createStudent(StudentList *student, char *name, int id, float *grades, int *index);
+void readStudent(StudentList *student, int *index);
+void updateStudentGrades(StudentList *student, int updateValue, float *newGrade, int *index);
 
+int main(){
+    int size = 3;          // Variable to hold the current number of students
+    int arraySize = 5; // Fixed array size for demonstration purposes
+    int index = 0;
+    
+    StudentList *student = malloc(sizeof(student) * size);
+    
+    float stud1Grade[NUM_GRADES] = {85.00, 90.00, 78.50, 88.00, 92.50};
+    float stud2Grade[NUM_GRADES] = {70.00, 75.00, 80.00, 82.00, 79.00};
+    float stud3Grade[NUM_GRADES] = {95.00, 93.50, 97.00, 91.00, 89.50};
+    float newGrade[NUM_GRADES] = {88.00, 90.00, 85.50, 87.00, 89.00};
 
-int main (){
+    createStudent(student, "Alice Johnson", 101, stud1Grade, &index);
+    createStudent(student, "Bob Smith", 102, stud2Grade, &index);
+    createStudent(student, "Carol Davis", 103, stud3Grade, &index);
 
+    printf("Students in the system:\n");
+    readStudent(student, &index);
+
+    printf("\n");
+
+    updateStudentGrades(student, 102, newGrade, &index);
+    readStudent(student, &index);
+
+    free(student);
+
+    // printf("%s", student[0].name);
+    // printf("%d", student[0].studentID);
+    // printf("%.2f", student[0].grade[0]);
 
     return 0;
 }
 
+void updateStudentGrades(StudentList *student, int ID, float *newGrade, int *index){
+    for(int i = 0; i < *index; i++)
+    {
+        if(student[i].studentID == ID)
+        {
+            printf("After updating %s's grades:\n", student[i].name);
+            memcpy(student[i].grade, newGrade, NUM_GRADES * sizeof(float));
+        }
+    }
+}
 
+float calculateAverage(float *grades){
+    float avg;
+    float sum = 0;
 
+    for(int i = 0; i < NUM_GRADES; i++)
+    {
+        sum += grades[i];
+    }
+    return avg = sum / NUM_GRADES;
+}
 
+void createStudent(StudentList *student, char *name, int id, float *grades, int *index){
+    strcpy(student[(*index)].name, name);
+    student[(*index)].studentID = id;
+    memcpy(student[(*index)].grade, grades, NUM_GRADES * sizeof(int));
 
+    (*index)++;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void readStudent(StudentList *student, int *index){
+    for(int i = 0; i < *index; i++)
+    {
+        printf("Student %d: Name = %s, ID = %d, Grade = ", i+1, student[i].name, student[i].studentID);
+        for(int j = 0; j < NUM_GRADES; j++)
+        {
+            printf("%.2f ", student[i].grade[j]);
+        }
+        float average = calculateAverage(student[i].grade);
+        printf("Average = %.2f", average);
+        printf("\n");
+    }
+}
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <string.h>
-// #define MAX 20
-
-
-// typedef struct{
-//     char fname[MAX];
-//     char lname[MAX];
-//     char Mi;
-// }Name;
+// #define MAX 50
+// #define NUM_GRADES 5
 
 // typedef struct{
-//     char month[MAX];
-//     int day;
-//     int year;
-// }BirthDate;
+//     char name[MAX];
+//     int studentID;
+//     float grade[NUM_GRADES];
+// }Students;
 
-// typedef struct{
-//     char department[MAX];
-//     char location[MAX];
-// }Department;
-
-// typedef struct {
-//     Name name;
-//     BirthDate birthDate;
-//     Department department;
-//     char school[MAX];
-// }Student;
-
-// typedef struct {
-//     Student *student;
-//     int idnum;
-// }StudentList;
-
-// Department createDepartment(char *department, char *location);
-// BirthDate createBirthdate(char *month, int day, int year);
-// Name createName(char *lname, char *fname, char mi);
-// Student createStudent(Name name, BirthDate birthday, Department department, char *school);
-// void addStudent(StudentList *Stud, Student newStud);
+// void createStudents(Students *studentList, char *name, int ID, float *grades, int *index);
+// void display(Students *studentList, int *index);
+// float calculateAverage(Students *studentList, int *index);
 
 // int main(){
-//     StudentList Stud;
-//     Stud.student = malloc(sizeof(Student) * MAX);
-//     Stud.idnum = 8;
-
-//     Student stuD1 = createStudent(createName("Jumong", "Jimping", 'I'), createBirthdate("September", 8, 2004),createDepartment("DCISM", "4th Floor"), "San Carlos");
-
-//     printf("%s %s %c\n", stuD1.name.lname, stuD1.name.fname, stuD1.name.Mi);
-//     printf("%s %d %d\n", stuD1.birthDate.month, stuD1.birthDate.day, stuD1.birthDate.year);
-//     printf("%s %s %s\n", stuD1.department.department, stuD1.department.location, stuD1.school);
-
+//     int size;          // Variable to hold the current number of students
+//     int arraySize = 5; // Fixed array size for demonstration purposes
+//     int index = 0;
+    
+//     Students *studentList = malloc(sizeof(studentList) * 3);
+    
+//     float stud0Grade[NUM_GRADES] = {85.00, 90.00, 78.50, 88.00, 92.50};
+//     float stud1Grade[NUM_GRADES] = {85.00, 90.00, 78.50, 88.00, 92.50};
+//     float stud2Grade[NUM_GRADES] = {70.00, 75.00, 80.00, 82.00, 79.00};
+//     float stud3Grade[NUM_GRADES] = {95.00, 93.50, 97.00, 91.00, 89.50};
+    
+//     createStudents(studentList, "Alice Johnson", 101, stud0Grade, &index);
+//     createStudents(studentList, "Alice Johnson", 101, stud1Grade, &index);
+//     createStudents(studentList, "Bob Smith", 102, stud2Grade, &index);
+//     createStudents(studentList, "Carol Davis", 103, stud3Grade, &index);
+    
+//     printf("Students in the system:\n");
+    
+//     display(studentList, &index);
+    
+//     // printf("%.2f\n", studentList[0].grade[0]);
+//     // printf("%s\n", studentList[1].name);
+//     // printf("%d\n", studentList[0].studentID);
+//     // printf("%d\n", studentList[0].studentID);
+    
 //     return 0;
 // }
 
-// Name createName(char *lname, char *fname, char mi){
-//     Name newName;
-//     strcpy(newName.lname, lname);
-//     strcpy(newName.fname, fname);
-//     newName.Mi = mi;
-
-//     return newName;
-// }
-// BirthDate createBirthdate(char *month, int day, int year){
-//     BirthDate newBirthday;
-//     strcpy(newBirthday.month, month);
-//     newBirthday.day = day;
-//     newBirthday.year = year;
-
-//     return newBirthday;
-// }
-// Department createDepartment(char *department, char *location){
-//     Department newDepartment;
-//     strcpy(newDepartment.department, department);
-//     strcpy(newDepartment.location, location);
-
-//     return newDepartment;
-// }
-// Student createStudent(Name name, BirthDate birthday, Department department, char *school){
-//     Student newStudent;
-//     newStudent.name = name;
-//     newStudent.birthDate = birthday;
-//     newStudent.department = department;
-//     strcpy(newStudent.school, school);
-
-//     return newStudent;
-// }
-
-
-// void addStudent(StudentList *Stud, Student newStud){
+// float calculateAverage(Students *studentList, int *index){
+//     float avg;
+//     float sum = 0;
+//     for(int i = 0; i < 5; i++)
+//     {
+//         if(i != 0){
+//             sum += studentList->grade[i];
+//         }
+//     }
+//     avg = sum / 5;
     
+//     return avg;
+// }
+// float calculateAverage(float* grades){
+//     float avg;
+//     float sum = 0;
+//     for(int i = 0; i < 5; i++)
+//     {
+//         if(i != 0){
+//             sum += grades[i];
+//         }
+//     }
+//     avg = sum / 5;
+    
+//     return avg;
+// }
+
+// void createStudents(Students *studentList, char *name, int ID, float *grades, int *index){
+//     strcpy(studentList[(*index)].name, name);
+//     studentList[(*index)].name[strcspn(studentList[(*index)].name, "\n")] = '\0';
+//     studentList[(*index)].studentID = ID;
+    
+//     for(int i = 0; i < 5; i++)
+//     {
+//             studentList[(*index)].grade[i] = grades[i];
+//     }
+//     (*index)++;
+// }
+
+// void display(Students *studentList, int *index){
+//     for(int i = 0; i < 4; i++){
+//         if(i != 0){
+//         printf("Student %d:", i);
+//         printf(" Name = %s,", studentList[i].name);
+//         printf(" ID = %d", studentList[i].studentID);
+//         printf(" Grade = ");
+//         for(int j = 0; j < 5; j++)
+//         {
+//             printf("%.2f", studentList[i].grade[j]);    
+//         }
+//         printf("Average = ");
+//         float average = calculateAverage(studentList, index);
+//         printf("%.2f", average);
+        
+//         printf("\n");
+//         }
+//     }
 // }
