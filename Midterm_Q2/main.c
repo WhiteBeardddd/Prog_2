7135275bd2abde16344152f487db90778e5cc413
Charlesm <charlesbenedictboquecosa@gmail.com>
2024-10-05 14:21:52 +0800

    Init

--- Midterm_Q2/main.c
+++ Midterm_Q2/main.c
@@ -1,9 +1,63 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
+typedef struct{
+	char fName[32];
+	char mi;
+	char lName[16];
+}fullName;
 
-    
-    return 0;
+typedef struct{
+	int score;			//Score of the student.
+	int totalScore;		//Total score of the exam.
+}exam;
+
+typedef struct{
+	exam exams[4];
+	int numExams;
+}grades;
+
+typedef struct{
+	fullName name;
+	char idNum[9];
+	char course[5];
+	char gender;
+	grades examScores;
+}student;
+
+typedef struct{
+	char idNum[9];
+	int index;
+}studIndex;
+
+studIndex* getExamPassers(student*, int, int);
+
+int main(void)
+{
+	student students[10] = {
+	    {{"Alice", 'B', "Johnson"}, "22101234", "BSCS", 'F', {{{85, 100}, {75, 100}, {60, 100}, {90, 100}}, 4 }},
+	    {{"Bob", 'C', "Smith"}, "21102345", "BSIT", 'M', {{{45, 100}, {70, 100}, {80, 100}, {65, 100}}, 4 }},
+	    {{"Charlie", 'D', "Brown"}, "20103456", "BSIS", 'M', {{{78, 100}, {60, 100}, {85, 100}, {55, 100}}, 4 }},
+	    {{"Diana", 'E', "Clark"}, "19104567", "BSCS", 'F', {{{60, 100}, {62, 100}, {90, 100}, {50, 100}}, 4 }},
+	    {{"Ethan", 'F', "Lewis"}, "18105678", "BSIT", 'M', {{{70, 100}, {80, 100}, {90, 100}, {30, 100}}, 4 }},
+	    {{"Fiona", 'G', "Martinez"}, "22106789", "BSIS", 'F', {{{85, 100}, {88, 100}, {90, 100}, {40, 100}}, 4 }},
+	    {{"George", 'H', "Wilson"}, "21107890", "BSCS", 'M', {{{67, 100}, {75, 100}, {80, 100}, {90, 100}}, 4 }},
+	    {{"Hannah", 'I', "Moore"}, "20108901", "BSIT", 'F', {{{55, 100}, {60, 100}, {85, 100}, {95, 100}}, 4 }},
+	    {{"Ian", 'J', "Taylor"}, "19109012", "BSIS", 'M', {{{30, 100}, {40, 100}, {60, 100}, {70, 100}}, 4 }},
+	    {{"Julia", 'K', "Anderson"}, "18101234", "BSCS", 'F', {{{90, 100}, {85, 100}, {70, 100}, {40, 100}}, 4 }}
+	};
+	int numStuds = sizeof(students) / sizeof(student);
+
+	studIndex* exam1Passers;
+	studIndex* exam2Passers;
+	studIndex* exam3Passers;
+	studIndex* exam4Passers;
+	studIndex* exam5Passers;
+
+
+    printf("Enter ");
+
+
+	return 0;
 }
