734ec480af6403502f9d4cea0a1fca6baab2a90d
Charlesm <charlesbenedictboquecosa@gmail.com>
2024-10-06 21:11:13 +0800

    sizeof check

--- Midterm_Q2/tester.c
+++ Midterm_Q2/tester.c
@@ -9,8 +9,8 @@ typedef struct{
 }fullName;
 
 typedef struct{
-	int score;			//Score of the student.
-	int totalScore;		//Total score of the exam.
+	int score;			// Score of the student.
+	int totalScore;		// Total score of the exam.
 }exam;
 
 typedef struct{
@@ -31,19 +31,40 @@ typedef struct{
 	int index;
 }studIndex;
 
-studIndex* getExamPassers(student *students, int numStuds, int examNum);
+studIndex* getExamPassers(student* students, int numStudents, int examNum) {
+	// Allocate memory for the passers list
+	studIndex* passers = (studIndex*)malloc((numStudents + 1) * sizeof(studIndex)); // +1 for sentinel
+	int count = 0;
 
-studIndex* getExamPassers(student *students, int numStuds, int examNum){
-    
-    studIndex *passers = malloc(sizeof()*);
-    
+	// Iterate through students and check scores for the specified exam
+	for (int i = 0; i < numStudents; i++) {
+		int score = students[i].examScores.exams[examNum].score;
+		int total = students[i].examScores.exams[examNum].totalScore;
+		// If score is at least 60% of the total score
+		if (score >= total * 0.6) {
+			// Copy the student's ID and index to passers
+			strcpy(passers[count].idNum, students[i].idNum);
+			passers[count].index = i;
+			count++;
+		}
+	}
+	// Set sentinel value
+	passers[count].index = -1;
+	return passers;
 }
 
-int main(void)
-{
+void printPassers(studIndex* passers) {
+	int i = 0;
+	while (passers[i].index != -1) {
+		printf("Student ID: %s, Index: %d\n", passers[i].idNum, passers[i].index);
+		i++;
+	}
+}
+
+int main(void) {
 	student students[10] = {
 	    {{"Alice", 'B', "Johnson"}, "22101234", "BSCS", 'F', {{{85, 100}, {75, 100}, {60, 100}, {90, 100}}, 4 }},
-	    {{"Bob", 'C', "Smith"}, "21102345", "BSIT", 'M', {{{45, 100}, {70, 100}, {80, 100}, {65, 100}}, 4 }},
+	    {{"Bob", 'C', "Smith"}, "20102345", "BSIT", 'M', {{{45, 100}, {70, 100}, {80, 100}, {65, 100}}, 4 }},
 	    {{"Charlie", 'D', "Brown"}, "20103456", "BSIS", 'M', {{{78, 100}, {60, 100}, {85, 100}, {55, 100}}, 4 }},
 	    {{"Diana", 'E', "Clark"}, "19104567", "BSCS", 'F', {{{60, 100}, {62, 100}, {90, 100}, {50, 100}}, 4 }},
 	    {{"Ethan", 'F', "Lewis"}, "18105678", "BSIT", 'M', {{{70, 100}, {80, 100}, {90, 100}, {30, 100}}, 4 }},
@@ -53,21 +74,17 @@ int main(void)
 	    {{"Ian", 'J', "Taylor"}, "19109012", "BSIS", 'M', {{{30, 100}, {40, 100}, {60, 100}, {70, 100}}, 4 }},
 	    {{"Julia", 'K', "Anderson"}, "18101234", "BSCS", 'F', {{{90, 100}, {85, 100}, {70, 100}, {40, 100}}, 4 }}
 	};
-
 	int numStuds = sizeof(students) / sizeof(student);
 
-	studIndex* exam1Passers;
 
-    printf("Code");
+	// Find and print passers for each exam
+	for (int exam = 0; exam < 4; exam++) {
+		printf("Passers for Exam %d:\n", exam + 1);
+		studIndex* passers = getExamPassers(students, numStuds, exam);
+		printPassers(passers);
+		free(passers); // Free the allocated memory
+		printf("\n");
+	}
 
 	return 0;
 }
-
-// Write the code for the function getExamPassers().
-// The function will receive an array of students, the number of students, 
-// and an integer from 0-3 which indicates which of the four major exams to check. 
-// The function will iterate through the list of students to find their scores for 
-// the specified exam. The function will store into an array of studIndex the idnumbers 
-// and indices of the students who got a score
-// that is at least 60% of the total score. Return the array of studIndex to the calling function. Let the member "index"
-// of the array of studIndex contain a -1 to serve as a sentinel value.
