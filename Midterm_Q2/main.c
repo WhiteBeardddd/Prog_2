3d73f22021969df579bb80c397e9f0b48841f590
Charlesm <charlesbenedictboquecosa@gmail.com>
2024-10-08 11:07:43 +0800

    Midterm Exam 2

--- Midterm_Q2/main.c
+++ Midterm_Q2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_STUD 10
 
 typedef struct{
 	char fName[32];
@@ -16,6 +17,7 @@ typedef struct{
 typedef struct{
 	exam exams[4];
 	int numExams;
+    int numPassedExams;
 }grades;
 
 typedef struct{
@@ -27,21 +29,17 @@ typedef struct{
 }student;
 
 typedef struct{
-	char idNum[9];
+    student *studPass;
 	int index;
 }studIndex;
 
-studIndex* getExamPassers(student *students, int numStuds, int examNum);
-
-studIndex* getExamPassers(student *students, int numStuds, int examNum){
-    
-    studIndex *passers = malloc(sizeof()*);
-    
-}
+studIndex getExamPassers(student *students, int numStuds);
+void init_student_index(studIndex *array);
+void display(studIndex passers);
 
 int main(void)
 {
-	student students[10] = {
+	student students[MAX_STUD] = {
 	    {{"Alice", 'B', "Johnson"}, "22101234", "BSCS", 'F', {{{85, 100}, {75, 100}, {60, 100}, {90, 100}}, 4 }},
 	    {{"Bob", 'C', "Smith"}, "21102345", "BSIT", 'M', {{{45, 100}, {70, 100}, {80, 100}, {65, 100}}, 4 }},
 	    {{"Charlie", 'D', "Brown"}, "20103456", "BSIS", 'M', {{{78, 100}, {60, 100}, {85, 100}, {55, 100}}, 4 }},
@@ -53,15 +51,66 @@ int main(void)
 	    {{"Ian", 'J', "Taylor"}, "19109012", "BSIS", 'M', {{{30, 100}, {40, 100}, {60, 100}, {70, 100}}, 4 }},
 	    {{"Julia", 'K', "Anderson"}, "18101234", "BSCS", 'F', {{{90, 100}, {85, 100}, {70, 100}, {40, 100}}, 4 }}
 	};
+	int numStuds = MAX_STUD;
 
-	int numStuds = sizeof(students) / sizeof(student);
+    studIndex PASS;
+    init_student_index(&PASS);
 
-	studIndex* exam1Passers;
-
-    printf("Code");
+    PASS = getExamPassers(students, numStuds);
+    display(PASS);
 
 	return 0;
 }
+
+void init_student_index(studIndex *array) {
+    array->index = 0;
+    array->studPass = malloc(MAX_STUD * sizeof(student));
+}
+
+
+studIndex getExamPassers(student *students, int numStuds){
+    student temp[MAX_STUD];
+    int index = 0;
+    for(int i = 0; i < numStuds; i++)
+    {
+        students[i].examScores.numPassedExams = 0;
+        for (int examNum = 0; examNum < 4; ++examNum) 
+        {
+                if(students[i].examScores.exams[examNum].score >= students[i].examScores.exams[examNum].totalScore * 0.6)
+                {
+                    students[i].examScores.numPassedExams++;
+                }
+        }
+        if (students[i].examScores.numPassedExams > 0) {
+            temp[index] = students[i];
+            index++;
+        }
+    }
+    studIndex passers;
+    passers.studPass = malloc(sizeof(student) * index);
+    if (passers.studPass) {
+        memcpy(passers.studPass, temp, sizeof(student) * index);
+        passers.index = index;
+    }
+    return passers;
+}
+
+void display(studIndex passers){
+    for(int i = 0; i < passers.index; i++)
+    {
+        for (int j = 0; j < passers.studPass[i].examScores.numPassedExams; ++j) {
+            printf("ID: %s | Name: %s %c %s | Program: %s | Scores: %d \n", 
+            passers.studPass[i].idNum, 
+            passers.studPass[i].name.fName, 
+            passers.studPass[i].name.mi,
+            passers.studPass[i].name.lName,
+            passers.studPass[i].course,
+            passers.studPass[i].examScores.exams[j]);
+
+        }
+    }
+}
+
 
 // Write the code for the function getExamPassers().
 // The function will receive an array of students, the number of students, 
